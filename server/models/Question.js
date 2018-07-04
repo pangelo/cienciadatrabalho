a76a25b44b3456a07bb353844f8d7da005aa95e2
Christian Marques <christian.marques@gmail.com>
2018-07-04 11:37:21 +0100

    App Skeleton

---
+++ server/models/Question.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var questionSchema = new mongoose.Schema({
+	question: { type: String, required:true},
+	created_at: { type: Date, default:Date.now },
+    interactiveStory : { type:String },
+    poster: { type: String},
+    details: { type: String}
+});
+
+questionSchema.pre('save', function(next){
+  now = new Date();
+  this.created_at = now;
+  next();
+});
+
+//questionSchema.index({ "title": 'text', "body": 'text', "author.name": 'text', "recap": 'text', "categories.tag": 'text' });
+
+module.exports = mongoose.model('Question', questionSchema);
